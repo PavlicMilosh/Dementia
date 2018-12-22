ff960c0840f1b27f72de5be9df4cd6cd78920c7c
unknown <milosh.p@live.com>
2018-12-22 16:52:57 +0100

    Finished preprocessing of merged dataset

--- data_preprocessing/merged.py
+++ data_preprocessing/merged.py
@@ -1,6 +1,9 @@
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.model_selection import train_test_split
+import itertools
 
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+
+from data_preprocessing.basic_preprocessing import remove_others
 from data_preprocessing.basic_preprocessing import load_data
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.impute import SimpleImputer
@@ -22,28 +25,19 @@ def preprocess_merged_models_imputing(X):
     pass
 
 
-if __name__ == '__main__':
-    merged = load_data()["merged"]
-    merged.dropna(axis="rows", subset=["dx1"], inplace=True)
-    merged.drop(labels=["ID", "Subject"], axis="columns", inplace=True)
-
-    # Deal with missing values
-    merged = preprocess_merged_remove_rows(merged)
-
+def split_and_encode(dataset, y_label):
     # Get X and y parts of the dataset
-    y = merged["dx1"]
-    X = merged.drop(labels="dx1", axis="columns")
+    y = dataset[y_label]
+    X = dataset.drop(labels=y_label, axis="columns")
 
     # Encode dx1 (target variable)
     y_encoder = LabelEncoder()
     y = y_encoder.fit_transform(y)
 
-    # Train test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    return X, y
 
-    # Perform z-score normalization
-    scaler = StandardScaler().fit_transform(X_train)
 
+def filter_by_feature_importance(threshold, dataset, X_train, y_train):
     # Feature importance
     forest = ExtraTreesClassifier(n_estimators=500, random_state=0)
 
@@ -57,15 +51,54 @@ if __name__ == '__main__':
 
     print("Feature ranking:")
     for f in range(X.shape[1]):
-        print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+        print("%d. feature %s (%f)" % (f + 1, list(dataset)[indices[f]], importances[indices[f]]))
 
     # Plot the feature importances of the forest
     plt.figure()
     plt.title("Feature importances")
-    plt.bar(range(X.shape[1]), importances[indices],
+    plt.bar(range(X_train.shape[1]), importances[indices],
             color="r", yerr=std[indices], align="center")
-    plt.xticks(range(X.shape[1]), indices)
-    plt.xlim([-1, X.shape[1]])
+    plt.xticks(range(X_train.shape[1]), indices)
+    plt.xlim([-1, X_train.shape[1]])
     plt.show()
 
+    features_to_keep = set(itertools.compress(list(dataset), [i > threshold for i in importances]))
+    features_to_keep.add('dx1')
+
+    return remove_others(dataset, features_to_keep)
+
+
+if __name__ == '__main__':
+    merged = load_data()["merged"]
+    merged.dropna(axis="rows", subset=["dx1"], inplace=True)
+    merged.drop(labels=["ID", "Subject"], axis="columns", inplace=True)
+
+    X, y = split_and_encode(merged, "dx1")
+
+    # Train test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    # Perform z-score normalization
+    scaler = StandardScaler().fit_transform(X_train)
+
+    merged = filter_by_feature_importance(0.005, merged, X_train, y_train)
+
+    X, y = split_and_encode(merged, "dx1")
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
     # Try out different models and see their performance
+
+    seed = 14
+
+    #Bagging
+    rsCV = RandomizedSearchCV()
+
+    #RandomForest
+
+    #ExtraTrees
+
+    #GradientBoosting
+
+    #XGBoost
+
